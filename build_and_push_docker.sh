7596ded349b0b0719f63df4e5d6ed80243621fa8
glennbech <glenn.bech@gmail.com>
2020-09-14 11:49:32 +0200

    Dont run it: -)

--- build_and_push_docker.sh
+++ build_and_push_docker.sh
@@ -3,4 +3,3 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build . --tag pgr301-pingpong --build-arg JAR_FILE=./target/travisdemo-0.0.1-SNAPSHOT.jar
 docker tag pgr301-pingpong  glennbech/pgr301-pingpong
 docker push glennbech/pgr301-pingpong
-docker run -p 8080:8080 glennbech/pgr301-pingpong
